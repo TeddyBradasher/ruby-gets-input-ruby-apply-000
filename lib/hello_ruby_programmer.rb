bb019e49ca54da8033f9a8ce6d391fbd136bddb9
Theodore Ray Bradsher <teddybradsher@gmail.com>
2020-05-17 14:42:51 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,3 @@
 def greeting
-  puts "Hi, #{name["you"]}! Welcome to the wonderful world of Ruby programming."
+  puts "Hi, #{name["ruby programmer"]}! Welcome to the wonderful world of Ruby programming."
 end
